6553dfd1a8e983dfa60d87583a1f4cdf445d0deb
juespega <juespega666@gmail.com>
2024-08-18 13:13:37 -0500

    feat: Add encryption and decryption functionality
    The code changes include adding encryption and decryption functionality to the application. This allows users to encrypt and decrypt text using a specific pattern. The functions `btnEncrypt` and `btnDecrypt` handle the encryption and decryption process respectively. The `encrypt` function replaces each letter in the input text with a corresponding pattern, while the `decrypt` function reverses the process. The encrypted or decrypted text is displayed in the output text area.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const textArea = document.querySelector(".text-area");
-const mensaje = document.querySelector(".mensaje");
-const btnCopiarElemento = document.querySelector(".btn-copiar");
-
+// Selecciona los elementos del DOM necesarios para la funcionalidad
+const inputText = document.querySelector("#text");
+const outputText = document.querySelector("#out_text");
+const btnCopyElement = document.querySelector("#copy");
 
 // La letra "e" es convertida para "enter"
 // La letra "i" es convertida para "imes"
@@ -9,48 +9,94 @@ const btnCopiarElemento = document.querySelector(".btn-copiar");
 // La letra "o" es convertida para "ober"
 // La letra "u" es convertida para "ufat"
 
+// Función para manejar la encriptación del texto
+function btnEncrypt() {
+  // Encripta el texto de entrada
+  const encryptText = encrypt(inputText.value);
+  // Muestra el texto encriptado en el textarea de salida
+  outputText.value = encryptText;
+  // Limpia el textarea de entrada
+  inputText.value = "";
 
-function btnEncriptar(){
-    const textoEncriptado = encriptar(textArea.value);
-    mensaje.value = textoEncriptado;
-    textArea.value = "";
-    mensaje.style.backgroundImage = "none";
+  // Ocultar la imagen
+  const imgElement = document.querySelector(".output__text__img img");
+  if (imgElement) {
+    imgElement.style.display = "none";
+  }
+
+  // Ocultar las etiquetas <p> dentro de la clase "content"
+  const paragraphs = document.querySelectorAll(".content p");
+  paragraphs.forEach((p) => {
+    p.style.display = "none";
+  });
 }
 
-function encriptar(stringEncriptada){
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase();
+// Función que realiza la encriptación de acuerdo a un patrón específico
+function encrypt(encryptedString) {
+  let matrixCode = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  // Convierte el texto a minúsculas
+  encryptedString = encryptedString.toLowerCase();
 
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringEncriptada.includes(matrizCodigo[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
-        }
+  // Reemplaza cada letra según el patrón de encriptación
+  for (let i = 0; i < matrixCode.length; i++) {
+    if (encryptedString.includes(matrixCode[i][0])) {
+      encryptedString = encryptedString.replaceAll(
+        matrixCode[i][0],
+        matrixCode[i][1]
+      );
     }
-    return stringEncriptada;
+  }
+  return encryptedString;
 }
 
-
-function btnDesencriptar(){
-    const textoEncriptado = desencriptar(textArea.value);
-    mensaje.value = textoEncriptado;
-    textArea.value = "";
+// Función para manejar la desencriptación del texto
+function btnDecrypt() {
+  const encryptText = decrypt(inputText.value);
+  outputText.value = encryptText;
+  inputText.value = "";
 }
 
-function desencriptar(stringDesencriptada){
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringDesencriptada = stringDesencriptada.toLowerCase();
+// Función que realiza la desencriptación de acuerdo al patrón inverso
+function decrypt(decryptedString) {
+  let matrixCode = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  // Convierte el texto a minúsculas
+  decryptedString = decryptedString.toLowerCase();
 
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringDesencriptada.includes(matrizCodigo[i][1])){
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
-        }
+  // Reemplaza cada patrón de encriptación con la letra correspondiente
+  for (let i = 0; i < matrixCode.length; i++) {
+    if (decryptedString.includes(matrixCode[i][1])) {
+      decryptedString = decryptedString.replaceAll(
+        matrixCode[i][1],
+        matrixCode[i][0]
+      );
     }
-    return stringDesencriptada;
+  }
+  return decryptedString;
 }
 
-// Funcionalidad del botón Copiar
-function btnCopiar() {
-    mensaje.select();
-    document.execCommand("copy");
-    alert("Texto copiado al portapapeles");
+// Función para copiar el texto encriptado/desencriptado al portapapeles
+function btnCopy() {
+  // Habilita temporalmente el textarea si está deshabilitado
+  outputText.disabled = false;
+  // Selecciona el textarea existente en el documento
+  const textToCopy = outputText;
+  // Selecciona el texto dentro del textarea
+  textToCopy.select();
+  // Ejecuta el comando de copiar
+  document.execCommand("copy");
+  // Deshabilita nuevamente el textarea
+  outputText.disabled = true;
+  alert("Text copied to clipboard");
 }
